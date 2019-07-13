c07234400fc115039fb1745c04f734db7bcbd346
Yurii Kniazyk <y.kniazyk@gmail.com>
2019-07-13 21:48:26 +0300

    png

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const opn = require('opn');
 const path = require('path');
 const error404 = require('./controller/error404');
 const config = require('./controller/config');
 const createTutor = require('./controller/DB/createTutor');
-const createStudent = require('./controller/DB/createStudent')
-const createSchool = require('./controller/DB/createSchool')
-const allStudents = require('./controller/allStudents')
-const allTutors = require('./controller/allTutors')
-
+const createStudent = require('./controller/DB/createStudent');
+const createSchool = require('./controller/DB/createSchool');
+const allStudents = require('./controller/allStudents');
+const allTutors = require('./controller/allTutors');
 
 mongoose.set('useFindAndModify', false);
 
@@ -18,14 +17,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'title/index.html'))
-});
+    res.sendFile(path.join(__dirname, 'title/index.html'))});
 app.post('/api/createStudent', createStudent);
 app.post('/api/createTutor', createTutor);
 app.post('/api/createSchool', createSchool);
-
-//app.get('/api/students', allStudents);
-//app.get('/api/tutors', allTutors);
+app.get('/api/students', allStudents);
+app.get('/api/tutors', allTutors);
 app.use('*', error404);
 
 app.listen(config.port, err => {
@@ -35,4 +32,3 @@ app.listen(config.port, err => {
         opn('http://localhost:' + config.port);
     }
 });
- 
